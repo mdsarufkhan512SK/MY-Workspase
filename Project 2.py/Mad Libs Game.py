c5f10b5b25cde1f44301b8a9601ca830fea7eebf
MD Saruf Khan <mdsarufkhan512@gmail.com>
2026-07-10 22:27:28 +0600

    update

---
+++ Project 2.py/Mad Libs Game.py
@@ -0,0 +1,8 @@
+username = input("Enter your name:")
+userfathername = input("Enter your fathername:")
+usermothername = input("Enter your mothername:")
+userage = input("Enter your Age:")
+
+
+
+print(f"My Dogys name is {username}","My Father name is {userfathername}","My Mother name is {usermothername},My dogys Age {userage}")
