ebcbb3fcd9bf5fc6972436cccb28781700b54a51
MD Saruf Khan <mdsarufkhan512@gmail.com>
2026-07-18 15:05:01 +0600

    update

---
+++ Project8.py/My desktop notifire.py
@@ -0,0 +1,3 @@
+from win10toast import ToastNotifier
+T = ToastNotifier()
+T.show_toast("Allah","Allah is watching me...!",duration=10)
---
+++ Project8.py/Python desktop notifier project.py
@@ -0,0 +1,11 @@
+from plyer import notification
+
+notification.notify(
+    title="Allah",
+    message="Allah is watching me..!",
+    app_name="Python 3.13",
+    timeout=60 # নোটিফিকেশনটি কত সেকেন্ড থাকবে
+)
+
+
+# ata gemini
